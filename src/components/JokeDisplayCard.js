a2e78c2cb45b0bd943ecb8d04edc1bcf0d128454
EdJaPe <ed.stu.ga.1214@gmail.com>
2022-02-27 17:18:35 -0800

    working on adding flipcard animation

--- src/components/JokeDisplayCard.js
+++ src/components/JokeDisplayCard.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
+
+const JokeDisplayCard = ({question, answer}) => {
+   const [flip, setFlip] = useState(false)
+
+  
 
 
-const JokeDisplayCard = ({question, answer, key}) => {
-const result = (answer==="") ? null:<h3>{answer}</h3>
   return(
-    <div className="jokeCard">
-      <h1>{key}</h1>
-      <h3>{question}</h3>
-      {result}
-    </div>
+    
+      <div 
+      className={`jokeCard ${flip ? 'is-flipped':''}`}
+      onClick={() => setFlip(!flip)} 
+      >
+        <div className="card--front">
+          <h3>{question}</h3>
+        </div>
+        <div className="card--back">
+          <h3>{answer}</h3>
+        </div>
+      </div>
+    
   )
 }
 export default JokeDisplayCard;
